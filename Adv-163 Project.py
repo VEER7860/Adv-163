89d6dafafcf40cab5734b05532bdfcbf424e0e82
VEER7860 <86776400+VEER7860@users.noreply.github.com>
2022-03-09 23:07:53 -0800

    Update Adv-163 Project.py

--- Adv-163 Project.py
+++ Adv-163 Project.py
@@ -2,46 +2,47 @@ from tkinter import *
 root = Tk()
 root.title("Heart_Diagnose_Report")
 root.geometry("600x630")
+root.configure(background="#fa8071")
 
 question1_radioButton=StringVar(value="0")
+question2_radioButton=StringVar(value="0")
+question3_radioButton=StringVar(value="0")
+question4_radioButton=StringVar(value="0")
+question5_radioButton=StringVar(value="0")
 
-Question1= Label(root, text ="Do you experience shortness of breath during routine activities? ")
+Question1=Label(root, text ="Do you experience shortness of breath during activities?")
 Question1.pack()
-question1_r1=Radiobutton(root, text ="yes",variable=question1_radioButton,value="yes" )
-question1_r1().pack
-question1_r2=Radiobutton(root, text ="no",variable=question1_radioButton,value="no" )
-question1_r2().pack()
+question1_r1=Radiobutton(root, text = "yes", variable=question1_radioButton, value="yes")
+question1_r1.pack()
+question1_r2=Radiobutton(root, text = "no", variable=question1_radioButton, value="no")
+question1_r2.pack()
 
-question2_radioButton=StringVar(value= "0")
-Question2=Label(root, text ="Do you experience shortness of breath while at rest/lying down?")
+Question2=Label(root, text ="Do you have a swelling in the feet/ankles/legs (shoes feel tighter) or abdomen?")
 Question2.pack()
-Question2_r1=Radiobutton(root, text ="yes",variable=question2_radioButton,value="yes" )
-Question2_r1.pack()
-question2_r2=Radiobutton(root, text ="no",variable=question2_radioButton,value="no" )
+question2_r1=Radiobutton(root, text = "yes", variable=question2_radioButton, value="yes")
+question2_r1.pack()
+question2_r2=Radiobutton(root, text = "no", variable=question2_radioButton, value="no")
 question2_r2.pack()
 
-question3_radioButton=StringVar(value= "0")
-Question3=Label(root, text ="Do you feel short of breath while lying flat and feel the need to stack multiple pillows to sleep well?")
+Question3=Label(root, text ="Do you feel any of the symptoms - confusion, disorientatione or loss of memory ?")
 Question3.pack()
-Question3_r1=Radiobutton(root, text ="yes",variable=question2_radioButton,value="yes" )
-Question3_r1.pack()
-question3_r2=Radiobutton(root, text ="no",variable=question2_radioButton,value="no" )
+question3_r1=Radiobutton(root, text = "yes", variable=question3_radioButton, value="yes")
+question3_r1.pack()
+question3_r2=Radiobutton(root, text = "no", variable=question3_radioButton, value="no")
 question3_r2.pack()
 
-question4_radioButton=StringVar(value= "0")
-Question4=Label(root, text ="Do you experience persistent wheezing / coughing that produces white or pink blood tinged mucus?")
+Question4=Label(root, text ="Do you experience shortness of breath while at rest/lying down?")
 Question4.pack()
-Question4_r1=Radiobutton(root, text ="yes",variable=question2_radioButton,value="yes" )
-Question4_r1.pack()
-question4_r2=Radiobutton(root, text ="no",variable=question2_radioButton,value="no" )
+question4_r1=Radiobutton(root, text = "yes", variable=question4_radioButton, value="yes")
+question4_r1.pack()
+question4_r2=Radiobutton(root, text = "no", variable=question4_radioButton, value="no")
 question4_r2.pack()
 
-question5_radioButton=StringVar(value= "0")
-Question5=Label(root, text ="Do you have swelling in the feet/ ankles/legs (shoes feel tighter) or abdomen?")
+Question5=Label(root, text ="Do you experience persistent weezing/coughing that produces white or pink blood tinged muscles?")
 Question5.pack()
-Question5_r1=Radiobutton(root, text ="yes",variable=question2_radioButton,value="yes" )
-Question5_r1.pack()
-question5_r2=Radiobutton(root, text ="no",variable=question2_radioButton,value="no" )
+question5_r1=Radiobutton(root, text = "yes", variable=question5_radioButton, value="yes")
+question5_r1.pack()
+question5_r2=Radiobutton(root, text = "no", variable=question5_radioButton, value="no")
 question5_r2.pack()
 
 def heart_score():
@@ -61,8 +62,8 @@ def heart_score():
     if question5_radioButton.get()=="yes":
         score = score+10
         print(score)
-
-if score <= 10:
+        
+    if score <= 10:
         messagebox.showinfo("Report","You don't need to visit a doctor.")
     elif score > 10 and score <= 30:
          messagebox.showinfo("Report","You might perhaps have to visit a doctor.")
@@ -71,4 +72,4 @@ if score <= 10:
 btn = Button(root, text= "Click me", command= heart_score)        
 btn.pack()
 root.mainloop()
-      
+
